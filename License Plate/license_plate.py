18520fe61b525f8a5cdcfe3dfc29bca7343c32e9
leoswaldo <lmaciasm10@gmail.com>
2015-04-07 21:24:28 -0500

    Adding template solution, pending to code main algoright

---
+++ License Plate/license_plate.py
@@ -0,0 +1,20 @@
+#!/python3/bin/python3
+
+
+## Function: find coincidences
+#  Description: find all coicidences of characters in the words_list of the
+#      license plate, except for dashes and return the number of coincidences
+#      in the fastest way (not case sensitive)
+#  Parameters: license_plate, words_list
+#      license_plate: characters to be searched
+#      words_list: list of string to search in
+def find_coincidences(license_plate, words_list):
+    # Pending to be coded
+    return 0
+
+
+if __name__ == '__main__':
+    license_plate = "PY-34-THON"
+    words_list = ["ASDF3490PASaTh", "TH3214Py0on", "TH3214Py0N", "TH3214Py0o"]
+
+    print(find_coincidences(license_plate, words_list))
